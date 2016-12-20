bc970c7d331f2aa7fece8c58724a40b6d699be88
Nicolas <nico2che@gmail.com>
2016-12-20 23:17:44 +0100

    iOS compatibility

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Navigator, TouchableOpacity } from 'react-native';
+import { Container, Content, Header, Title } from 'native-base';
 
-export default class netdevices extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+var ListPage = require('./ListPage');
+var DetailPage = require('./DetailPage');
+
+class NetDevices extends Component {
+    render() {
+        return (
+            <Navigator
+                initialRoute={{id: 'ListPage'}}
+                renderScene={this.renderScene}
+                configureScene={(route) => {
+                    if (route.sceneConfig) {
+                        return route.sceneConfig;
+                    }
+                    return Navigator.SceneConfigs.FloatFromRight;
+                }} />
+        );
+    }
+    renderScene(route, navigator) {
+        var routeId = route.id;
+
+        if (routeId === 'ListPage') {
+            return (
+                <ListPage
+                    {...route.passProps}
+                    navigator={navigator} />
+            );
+        }
+        if (routeId === 'DetailPage') {
+            return (
+                <DetailPage
+                    {...route.passProps}
+                    navigator={navigator} />
+            );
+        }
+        return (
+            <Container>
+              <Header>
+                <Title>404</Title>
+              </Header>
+              <Content>
+                <Text>
+                  Cette page n'existe pas
+                </Text>
+              </Content>
+            </Container>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('netdevices', () => netdevices);
+AppRegistry.registerComponent('netdevices', () => NetDevices);
